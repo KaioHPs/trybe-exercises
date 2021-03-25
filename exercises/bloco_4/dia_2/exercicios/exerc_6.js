5698aea0f1c176af470388d379dd2838103a0e98
KaioHPs <kaiohpsalternativo@gmail.com>
2021-03-25 19:28:17 -0300

    Exercicio 8

--- exercises/bloco_4/dia_2/exercicios/exerc_6.js
+++ exercises/bloco_4/dia_2/exercicios/exerc_6.js
@@ -1,7 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let oddNumbers = 0;
 
-for (let i = 0; i < numbers.length; i++) {
+for (let i = 0; i < numbers.length; i+=1) {
   if (numbers[i]%2!=0) {
     oddNumbers += 1
   } 
---
+++ exercises/bloco_4/dia_2/exercicios/exerc_8.js
@@ -0,0 +1,7 @@
+let oneTo25 = [];
+
+for (let i = 1; i <= 25; i+=1) {
+  oneTo25.push(i)
+}
+
+console.log(oneTo25);
