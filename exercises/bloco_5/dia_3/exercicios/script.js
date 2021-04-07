ca43ecf970d3cf230a9792b3dbefa396d28dcfc4
KaioHPs <kaiohpsalternativo@gmail.com>
2021-04-07 20:13:31 -0300

    Exercicio 2

--- exercises/bloco_5/dia_3/exercicios/script.js
+++ exercises/bloco_5/dia_3/exercicios/script.js
@@ -14,6 +14,7 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+// Exercicio 1
 function createDaysOfTheMonth() {
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
   const monthDaysList = document.querySelector('#days');
@@ -33,3 +34,14 @@ function createDaysOfTheMonth() {
 }
 
 createDaysOfTheMonth();
+
+// Exercicio 2
+function createHolidayButton(string) {
+  let buttonContainer = document.querySelector('.buttons-container');
+  let holidayButton = document.createElement('button');
+  holidayButton.innerText = string;
+  holidayButton.id = 'btn-holiday'
+  buttonContainer.appendChild(holidayButton);
+}
+
+createHolidayButton('Feriados');
